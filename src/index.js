c6e391b16d2512880d13c75cd489df6b21e6a915
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-13 15:34:26 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let newField = document.createElement("li")
   let content = document.createTextNode(e.target.children[1].value)
-  let removeBtn = document.createElement("Button")
+  let removeBtn = document.createElement("BUTTON")
+  removeBtn.innerHTML = "Remove"
 
   newField.appendChild(content, removeBtn);
   list.appendChild(newField)
