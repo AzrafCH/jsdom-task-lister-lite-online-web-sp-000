85f9469361d0844d15a14c634e524a8fea372c65
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-13 15:12:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const tasks = document.getElementById('tasks')
 
 taskForm.addEventListener("submit", (e) {
   e.preventDefault()
-  const taskInput = taskForm["new-task-description"].value 
+  const taskInput = taskForm["new-task-description"].value
 
   tasks.innerHTML += `<li id = "${taskInput}"> ${taskInput} </li>`
 
