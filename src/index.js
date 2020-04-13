7ee7c078aa9609704833526e28f6b13e85ccb9bc
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-13 15:37:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,17 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const taskForm = document.getElementById("create-task-form")
 
-function removeBtn () {
-
-}
-
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let newField = document.createElement("li")
   let content = document.createTextNode(e.target.children[1].value)
-  let removeBtn = document.createElement("BUTTON")
-  removeBtn.innerHTML = "Remove";
 
   newField.appendChild(content);
-  list.appendChild(newField, removeBtn)
+  list.appendChild(newField)
 })
