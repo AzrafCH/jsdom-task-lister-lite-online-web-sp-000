958b0bd3f42e59bf7dad79a4d29d285caeac7261
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-13 15:33:19 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let newField = document.createElement("li")
   let content = document.createTextNode(e.target.children[1].value)
+  let removeBtn = document.createElement("Button")
   newField.appendChild(content);
   list.appendChild(newField)
 })
